afa1c9b8d66f8d191772468d8ec688911f1516bb
getHard1226 <hi5.nakash1ma@gmail.com>
2018-10-26 22:22:46 +0900

    ダブルクオテーションでエラー

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang = "ja">
+<head>
+	<meta charset = "UTF-8">
+	<title>糞システム</title>
+</head>
+
+<body>
+<?php
+	echo "<form action = ¥"welcom.php¥" method = ¥"POST¥">";
+	echo "<table>";
+		echo "<tr>";
+			echo "<td>ユーザ名 : </td>";
+			echo "<td>"
+				echo "<input type = ¥"text¥" name = ¥"userName¥">";
+			echo "</td>"
+		echo "</tr>";
+
+		echo "<tr>";
+			echo "<td>パスワード : </td>";
+			echo "<td>";
+				echo "<input type = ¥"test¥" name = ¥"userPwd¥">";
+			echo "</td>";
+		echo "</tr>";
+	echo "</table>";
+	echo "</form>";
+?>
+</body>
+</html>
